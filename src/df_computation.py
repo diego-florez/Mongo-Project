6a0ef4efb425297f2082875a3e6e4e2a82dbb5f8
diego-florez <diegoflorezestrada@gmail.com>
2020-04-20 16:22:23 +0200

    modi

---
+++ src/df_computation.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+tech_comps = ["web","software","mobile","games_video","cleantech","biotech","netwok_hosting","analytics","search",
+              "hardware","nanotech"]
+
+dfo = pd.read_csv("../OUTPUT/dfo.csv", encoding = "latin-1")
+
+dfc = pd.read_csv("../OUTPUT/dfc.csv", encoding = "latin-1")
---
+++ src/functions.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from dotenv import load_dotenv
+from math import sqrt
+from pandas.io.json import json_normalize
+from df_computation import *
+
+
+def geoPlaces(address):
+    load_dotenv()
+    apiKey = os.getenv("google")
+    res = requests.get(f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius={r}&keyword={kw}&key={api_key}")
+    data = res.json()
+    return data
+
+
+def normalize(comps):
+    return json_normalize(comps, "offices", ["name","category_code","total_money_raised","founded_year"])
+
+
+def checkCoord(row):
+    lat = row.latitude
+    lng = row.longitude
+    lat1 = [e for e in dfo.latitude]
+    lng1 = [e for e in dfo.longitude]
+    a = [e - lat for e in lat1]
+    b = [e - lng for e in lng1]
+    c = [sqrt(e * e  +  i * i) for e,i in zip(a,b)]
+    radius = 2
+    test =  any(i <= radius for i in c)
+    return "inside" if test==True else "outside"
+
